5f32f752445f1f0f2e45109891d3e4060536da9d
Alex Ofori <alexofob@yahoo.com>
2013-06-12 22:13:25 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineAgent::Application.config.secret_token = 'e47249130d2e308d7867cda47ca1c32d9f848d439f0411bf486f472d8ec689da726501b440fa4cc33ce45f3a1c48572faf2f2c8af3f563e06e075c3326cf263b'
